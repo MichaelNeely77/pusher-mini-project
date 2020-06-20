93444a3163593d5669e857ca3d5f228b5667ad4e
Michael Neely <michael@michaelneely.net>
2020-06-19 23:33:36 -0400

    Added the Pusher module and changed structure of app

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var express = require('express')
 var app = express()
 
+
+
+var Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: '1022593',
+  key: '95ec86c745791e6b1b5f',
+  secret: '96c800ea3c091b91371d',
+  cluster: 'us2',
+  encrypted: true
+});
+
+pusher.trigger('my-channel', 'my-event', {
+  'message': 'hello world'
+});
+
 app.get('/', function(req,res) {
     res.send('Hello World2')
 })
