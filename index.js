823f56e07bc6cf5829d19404cd289edc62054d09
Michael Neely <michael@michaelneely.net>
2020-06-19 23:59:11 -0400

    Added teh post method and the comment route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 var Pusher = require('pusher');
 
 var pusher = new Pusher({
@@ -11,7 +12,13 @@ var pusher = new Pusher({
   encrypted: true
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.post('/comment', function (req, res) {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 pusher.trigger('my-channel', 'my-event', {
   'message': 'hello world'
